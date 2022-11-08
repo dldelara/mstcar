f6d599e10ec2594b2b31fdb4aabd69be4138e738
dldelara <dldelara57@gmail.com>
2022-11-08 08:12:18 -0500

    made progress updates more concise

--- src/progress.cpp
+++ src/progress.cpp
@@ -9,9 +9,7 @@ void progress(int s, int n_iter, int n, int l) {
     if (l > 0) Rcout << "Batch: " << n << "/" << l << progress.get_cstring() << "\r";
     else       Rcout << progress.get_cstring() << "\r";
   }
-  bool prog = false;
-  if ((s + 1) % (n_iter / 50) == 0) prog = true;
-  if (prog) {
+  if ((s + 1) % (n_iter / 50) == 0) {
     String progress(" Progress: |..................................................|");
     for (int i = 0; i < round((s + 1) * 50.0 / n_iter); i++) progress.replace_first(".", "*");
     if (l > 0) Rcout << "Batch: " << n << "/" << l << progress.get_cstring() << "\r";
